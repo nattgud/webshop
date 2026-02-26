e35782222373cab93d607254c638f2fe3531a320
david.c.e.andersson@gmail.com <david.c.e.andersson@gmail.com>
2026-02-26 17:59:30 +0100

    dotenv fix

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'webshop'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 module.exports = connection;
